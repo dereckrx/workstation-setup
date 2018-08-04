72d05a3393046dbade8d1bd3c0f20dd42adb71b9
Dereck Rasmussen <dereckrx@gmail.com>
2018-08-04 16:02:38 -0700

    Add intellij settings

--- scripts/intellij_rx.sh
+++ scripts/intellij_rx.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
-source ${MY_DIR}/scripts/common/download-pivotal-ide-prefs.sh
-pushd ~/workspace/pivotal_ide_prefs/cli
-./bin/ide_prefs install --ide=intellij
-popd
+#source ${MY_DIR}/scripts/common/download-pivotal-ide-prefs.sh
+#pushd ~/workspace/pivotal_ide_prefs/cli
+#./bin/ide_prefs install --ide=intellij
+#popd
+
+#<PRODUCT><VERSION>
+PRODUCT_VERSION=$(ls ~/Library/Preferences | grep IntelliJIdea)
+
+#Configuration (idea.config.path):
+#~/Library/Preferences/${PRODUCT_VERSION}
+
+#Plugins (idea.plugins.path):
+#~/Library/Application\ Support/${PRODUCT_VERSION}
+
+# Copy color scheme
+cp ${MY_DIR}/files/intellij/colors_rx.scheme.xml ${HOME}/Library/Preferences/${PRODUCT_VERSION}/options/colors.scheme.xml
+
+# Copy codestyles
+cp ${MY_DIR}/files/intellij/codestyles_rx.xml ${HOME}/Library/Preferences/${PRODUCT_VERSION}/codestyles/Default.xml
